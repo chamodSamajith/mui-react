889acb4d13e7410e3f760658778904a02fb3189a
Chamod Rathnayake <chamod.rathnayake@axiatadigitallabs.com>
2022-04-24 15:28:30 +0530

    completed upto home side bar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Container, Stack } from "@mui/material";
+import { Box } from "@mui/system";
+import Feed from "./Components/Feed";
+import NavBar from "./Components/NavBar";
+import RightBar from "./Components/RightBar";
+import SideBar from "./Components/SideBar";
+
+function App() {
+  return (
+    <Box>
+      <NavBar/>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <SideBar />
+        <Feed />
+        <RightBar />
+      </Stack>
+    </Box>
+  );
+}
+
+export default App;
